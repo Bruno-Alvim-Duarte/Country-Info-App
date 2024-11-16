69cc64ca758a137e7bde23cf6e83b268759c7afe
Bruno Duarte <brunoalvimduarte@gmail.com>
2024-11-16 19:18:33 -0300

    adding spinner to loading

--- frontend/src/app/countries/[:countryCode]/page.tsx
+++ frontend/src/app/countries/[:countryCode]/page.tsx
@@ -3,12 +3,13 @@ import useCountry from '@/client/hooks/useCountry';
 import Country from '@/components/Country/Country';
 import PopulationChart from '@/components/PopulationChart/PopulationChart';
 import Image from 'next/image';
+import { FaSpinner } from 'react-icons/fa6';
 
 const CountryPage = () => {
   const { countryData, loading } = useCountry();
   return loading ? (
-    <div>
-      <div>Loading...</div>
+    <div className="flex flex-col items-center mt-5">
+      <FaSpinner className="animate-spin w-8 h-8 text-3xl text-foreground" />
     </div>
   ) : (
     <div className="flex flex-col items-center gap-4 p-[2%]">
--- frontend/src/app/countries/page.tsx
+++ frontend/src/app/countries/page.tsx
@@ -2,13 +2,14 @@
 import useCountries from '@/client/hooks/useCountries';
 import Country from '@/components/Country/Country';
 import ICountry from '@/interfaces/ICountry';
+import { FaSpinner } from 'react-icons/fa6';
 
 const CountriesPage = () => {
   const { countries, loading } = useCountries();
 
   return loading ? (
-    <div>
-      <div>Loading...</div>
+    <div className="flex flex-col items-center mt-5">
+      <FaSpinner className="animate-spin w-8 h-8 text-3xl text-foreground" />
     </div>
   ) : (
     <div className="p-6 flex flex-col items-center gap-5">
